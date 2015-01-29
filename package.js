e855bd24f942e5e30efdd8a2654ccc23aa7f09fa
Daniel Dornhardt <daniel@dornhardt.com>
2015-01-29 12:11:33 +0100

    updating dependency versions

--- package.js
+++ package.js
@@ -1,12 +1,13 @@
 Package.describe({
     name: 'yagni:split-on-newlines',
-    version: '0.0.4',
+    version: '0.0.5',
     summary: "Helper Function and UI-Helper to split text on newlines.",
     git: "https://github.com/DanielDornhardt/yagni-split-on-newlines"
 });
 
 Package.onUse(function (api) {
-    api.use(['standard-app-packages', 'underscore', 'coffeescript']);
+    api.versionsFrom('METEOR@0.9.2');
+    api.use(['underscore', 'coffeescript']);
 
     api.export("splitOnNewlines");
 
